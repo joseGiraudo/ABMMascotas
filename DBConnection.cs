2d777ee4bb1330301ee99c01ca01e001d0582c58
alumno <alumno@920-M52>
2023-06-13 16:19:02 -0300

    Agregar archivos de proyecto.

---
+++ DBConnection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABMMascotas
+{
+    internal class DBConnection
+    {
+        private string connectionString = @"Data Source=172.16.10.196;Initial Catalog=Veterinaria404850;User ID=alumno1w1;Password=alumno1w1";
+
+        SqlConnection connection;
+
+        SqlCommand command;
+
+        // constructor
+        public DBConnection()
+        {
+            connection = new SqlConnection(connectionString);
+        }
+
+
+        // metodos
+        private void Conectar()
+        {
+            connection.Open();
+        }
+        public void Desconectar()
+        {
+            connection.Close();
+        }
+
+        public DataTable ConsultarBD(string SQLquery)
+        {
+            DataTable table = new DataTable(); // creo el dataTable
+
+            Conectar();
+            command = new SqlCommand(SQLquery, connection); // creo el comando con la consulta y la conexion
+
+            table.Load(command.ExecuteReader());  // cargo los datos en el dataTable
+
+            Desconectar();
+
+            return table;
+        }
+
+
+        // Insert Update Delete
+        public void IUD(string SQLquery)
+        {
+            Conectar();
+            command = new SqlCommand(SQLquery, connection);
+            command.ExecuteNonQuery();
+            Desconectar();
+        }
+
+        
+    }
+}
---
+++ frmMascota.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+//CURSO – LEGAJO – APELLIDO – NOMBRE
+
+namespace ABMMascotas
+{
+    public partial class frmMascota : Form
+    {
+       
+        DBConnection DBconn = new DBConnection();
+
+
+        public frmMascota()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMascota_Load(object sender, EventArgs e)
+        {
+            CargaCombo(cboEspecie, "Especies");
+            habilitar(false);
+            cargaLista(lstMascotas, "Mascotas");
+        }
+
+        private void CargaCombo(System.Windows.Forms.ComboBox combo, string nombreTabla) // le paso el nombre de la tabla de la cualq uiero traer los datos y el combo donde cargarlo
+        {
+            string query = "SELECT * FROM " + nombreTabla;
+
+            DataTable table = DBconn.ConsultarBD(query);
+
+            combo.DataSource = table;
+            combo.ValueMember = table.Columns[0].ColumnName;
+            combo.DisplayMember = table.Columns[1].ColumnName;
+            combo.DropDownStyle = ComboBoxStyle.DropDownList;
+
+        }
+
+        private void cargaLista(ListBox listBox, string nombreTabla)
+        {
+            listBox.Items.Clear();
+
+            string query = "SELECT * FROM " + nombreTabla;
+
+            DataTable table = DBconn.ConsultarBD(query);
+
+            foreach (DataRow row in table.Rows)
+            {
+                Mascota mascota = new Mascota();
+                mascota.Codigo = Convert.ToInt32(row[0]);
+                mascota.Nombre = row[1].ToString();
+                mascota.Especie = Convert.ToInt32(row[2]);
+                mascota.Sexo = Convert.ToInt32(row[3]);
+                mascota.FechaNacimiento = (DateTime)(row[4]);
+
+                listBox.Items.Add(mascota);
+
+            }
+
+        }
+
+        private void habilitar(bool x)
+        {
+
+            txtCodigo.Enabled = x;
+            txtNombre.Enabled = x;
+            cboEspecie.Enabled = x;
+            rbtMacho.Enabled = x;
+            rbtHembra.Enabled = x;
+            dtpFechaNacimiento.Enabled = x;
+
+            btnGrabar.Enabled = x;
+
+            btnNuevo.Enabled = !x;
+            lstMascotas.Enabled = !x;
+            
+
+        }
+
+        private void cargaForm()
+        {
+            if(lstMascotas.SelectedItems.Count > 0) 
+            {
+                Mascota mascota = (Mascota)lstMascotas.SelectedItem;
+
+                txtCodigo.Text = mascota.Codigo.ToString();
+                txtNombre.Text = mascota.Nombre.ToString();
+                cboEspecie.SelectedValue = mascota.Especie;
+                if(mascota.Sexo == 1)
+                {
+                    rbtMacho.Checked = true;
+                    rbtHembra.Checked= false;
+                }
+                else{
+                    rbtMacho.Checked = false;
+                    rbtHembra.Checked = true;
+                }
+                dtpFechaNacimiento.Value = mascota.FechaNacimiento;
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Desea salir del programa?", "Salir", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question) == DialogResult.Yes) 
+            {
+                this.Close();
+            }
+            
+            
+            
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            habilitar(true);
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            //valida datos...
+
+            //crear objeto
+            Mascota mascota = new Mascota();
+            mascota.Nombre = txtNombre.Text;
+            mascota.Especie = Convert.ToInt32(cboEspecie.SelectedValue);
+            if (rbtMacho.Checked)
+            {
+                mascota.Sexo = 1;
+            } else if (rbtHembra.Checked)
+            {
+                mascota.Sexo = 2;
+            }
+            mascota.FechaNacimiento = dtpFechaNacimiento.Value;
+
+            //insert usando parámetros
+            string insertQuery = "INSERT INTO Mascotas(nombre, especie, sexo, fechaNacimiento) " +
+                                                "VALUES(@nombre, @especie, @sexo, @fechaNacimiento)";
+
+            SqlCommand = new SqlCommand(insertQuery, connection);
+
+
+            DBconn.IUD(insertQuery);
+        }
+
+        private void lstMascotas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cargaForm();
+        }
+
+    }
+}
